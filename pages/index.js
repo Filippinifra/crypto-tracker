401106f183eafa285152eb42c3cc7d5ba75a66dd
filippinifra <filippinifra123@gmail.com>
2022-02-21 23:14:56 +0100

    try catch error fetching

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home({ prices }) {
+  if (!prices) {
+    return <div>No data!!</div>;
+  }
+
   const btc = prices.find(({ currency }) => currency === "BTC");
   const btcPrice = btc.price;
 
@@ -21,8 +26,14 @@ export default function Home({ prices }) {
 const FETCHING_SERVICE_API_KEY = "3d170cf90f2902990ba061da9eb39d4d275e3bd3";
 
 export async function getStaticProps() {
-  const res = await fetch(`https://api.nomics.com/v1/prices?key=${FETCHING_SERVICE_API_KEY}`);
-  const prices = await res.json();
+  let res = null;
+  try {
+    res = await fetch(`https://api.nomics.com/v1/prices?key=${FETCHING_SERVICE_API_KEY}`);
+  } catch (e) {
+    console.log(["error", e]);
+  }
+
+  const prices = res ? await res.json() : null;
 
   return {
     props: {
