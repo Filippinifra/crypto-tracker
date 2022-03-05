84f2feb1b25bc3354ae8b1e55f89d9e5057a6ba2
filippinifra <filippinifra123@gmail.com>
2022-03-05 10:14:59 +0100

    update hooks with correct types and values

--- src/hooks/useDetailedCoins.ts
+++ src/hooks/useDetailedCoins.ts
@@ -1,15 +1,16 @@
-import { Currency, getNameForCurrency } from "./../types/currency";
+import { Currency, getNameForCurrency } from "types/currency";
 import useSWR from "swr";
-import { DetailedCoins } from "types/detailedCoins";
 import { PersonalCoins } from "types/personalCoins";
+import { DetailedCoinsAPI } from "types/api/detailedCoinsAPI";
+import { toDetailedCoin } from "utils/coins";
 
 const fetcher = (options: any) => fetch(options).then((res) => res.json());
 
 export const useDetailedCoins = (coins: PersonalCoins | undefined, prefCurrency: Currency | undefined) => {
-  const { data } = useSWR<DetailedCoins>(
+  const { data } = useSWR<DetailedCoinsAPI>(
     coins?.length && prefCurrency !== undefined ? `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${getNameForCurrency(prefCurrency)}&ids=${coins.map(({ id }) => id).join(",")}` : null,
     fetcher
   );
 
-  return { detailedCoins: data };
+  return { detailedCoins: data ? toDetailedCoin(data) : null };
 };
--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -2,11 +2,12 @@ import { WalletDivision } from "types/walletDivision";
 import { useEffect, useState } from "react";
 
 const tempCoins: WalletDivision = [
-  { typology: "Liquidity", percentage: 40 },
+  { typology: "Liquidity", percentage: 50 },
   { typology: "Bigs", percentage: 25 },
-  { typology: "Alt cap", percentage: 10 },
-  { typology: "Mid cap", percentage: 15 },
-  { typology: "Low cap", percentage: 10 },
+  { typology: "High cap", percentage: 10 },
+  { typology: "Mid cap", percentage: 8 },
+  { typology: "Low cap", percentage: 4 },
+  { typology: "Gambling", percentage: 3 },
 ];
 
 export const useWallet = () => {
