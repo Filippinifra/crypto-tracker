f29d9bc0a88978cc8631ba6f482ab160220d1f41
filippinifra <filippinifra123@gmail.com>
2022-03-12 03:33:37 +0100

    remove warning for build

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,7 +2,7 @@ import IconMaterial from "@material-ui/core/Icon";
 import React, { FC } from "react";
 import { IconName } from "types/IconName";
 
-export const Icon: FC<{ style?: React.CSSProperties; name: IconName; color?: string; onClick: () => void }> = ({ style, name, color, onClick }) => {
+export const Icon: FC<{ style?: React.CSSProperties; name: IconName; color?: string; onClick?: () => void }> = ({ style, name, color, onClick }) => {
   return (
     <IconMaterial onClick={onClick} style={{ fontSize: 16, color: color || "black", ...style }}>
       {name}
