1afaaa5dddc8cd4c10ee906cb919b9a5815927c2
filippinifra <filippinifra123@gmail.com>
2022-03-13 16:55:25 +0100

    add data test to ux branch

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -5,32 +5,32 @@ import { useEffect, useState } from "react";
 const tempCoins: WalletDivision = [
   {
     typologyName: "Liquidity",
-    percentage: 50,
+    percentage: 30,
     typologyId: "deb967d1-148e-4ea0-bcdc-052e85e686e9",
   },
   {
     typologyName: "Bigs",
-    percentage: 25,
+    percentage: 30,
     typologyId: "09fb2a92-ff9d-4013-8506-0fc96adf1d6f",
   },
   {
     typologyName: "High cap",
-    percentage: 10,
+    percentage: 20,
     typologyId: "864b535f-f39a-42ed-a5ca-ecd6170a73f2",
   },
   {
     typologyName: "Mid cap",
-    percentage: 8,
+    percentage: 10,
     typologyId: "2e18de78-7db1-42b3-8dba-9094689aaeb5",
   },
   {
     typologyName: "Low cap",
-    percentage: 4,
+    percentage: 5,
     typologyId: "f5c213f7-6b1e-4ca3-a8f4-7b8e508076bd",
   },
   {
     typologyName: "Gambling",
-    percentage: 3,
+    percentage: 5,
     typologyId: "ac34932d-ab2a-4f45-b0ea-e769b80bf96f",
   },
 ].map((e, i) => {
