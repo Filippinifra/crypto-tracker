de0ea44ddd11f86236bc1245301126bef77e0d90
filippinifra <filippinifra123@gmail.com>
2022-03-15 21:22:39 +0100

    add useDimensions and useResponsive hooks

---
+++ src/hooks/useDimensions.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export const useDimensions = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    });
+
+    return () =>
+      window.addEventListener("resize", () => {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+      });
+  }, []);
+
+  return {
+    isSmallWidth: width < 600,
+    isMediumWidth: width < 900,
+    isLargeWidth: width >= 900,
+    width,
+    height,
+  };
+};
