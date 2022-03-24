03895ce990b56dc0ef52fe5e3c3066e0d634a78f
Marco <10533178@polimi.it>
2022-03-24 19:11:34 +0100

    password policy

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,2 +1,9 @@
 export const validateMail = (email: string) => /\S+@\S+\.\S+/.test(email);
 export const validatePassword = (password: string) => password.length >= 6;
+/* 
+
+/\d/.test(password) -> numero
+/^[A-Z]*$/.test(password) && /^[a-z]*$/.test(string) ->maiuscole e minuscole
+/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/.test(password) -> carattere speciale
+
+*/
