96c2cf07c9243f147ac0e5b20e317b45ef9e462b
filippinifra <filippinifra123@gmail.com>
2022-03-25 01:43:06 +0100

    handle disconnection in load error handler component

--- src/components/LoadErrorHandler.tsx
+++ src/components/LoadErrorHandler.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 import { errorColor, loadingColor } from "utils/colors";
 import { Typography } from "components/Typography";
@@ -13,6 +13,17 @@ const Wrapper = styled.div`
 `;
 
 export const LoadErrorHandler: FC<{ data: any; error: any }> = ({ data, error, children }) => {
+  const [isOnline, setOnline] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("offline", () => {
+      setOnline(false);
+    });
+    window.addEventListener("online", () => {
+      setOnline(true);
+    });
+  }, []);
+
   if (!data && !error) {
     return (
       <Wrapper style={{ backgroundColor: loadingColor, color: "black" }}>
@@ -21,10 +32,10 @@ export const LoadErrorHandler: FC<{ data: any; error: any }> = ({ data, error, c
     );
   }
 
-  if (error) {
+  if (error || !isOnline) {
     return (
       <Wrapper style={{ backgroundColor: errorColor, color: "white", display: "flex", flexDirection: "column", gap: 30 }}>
-        <Typography variant="body">Errore! Ricaricare la pagina.</Typography>
+        <Typography variant="body">{!isOnline ? "Non sei online, controlla la connessione!" : "Errore! Ricaricare la pagina."}</Typography>
         <ReloadPageButton />
       </Wrapper>
     );
