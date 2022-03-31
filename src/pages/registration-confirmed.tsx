108b55ec49e390cb062601fe6eeae3bd982cc58d
filippinifra <filippinifra123@gmail.com>
2022-04-01 01:21:11 +0200

    implement registration-confirmed path logic

--- src/pages/registration-confirmed.tsx
+++ src/pages/registration-confirmed.tsx
@@ -1,5 +1,36 @@
+import { LoadErrorHandler } from "components/LoadErrorHandler";
+import { applyActionCode } from "firebase/auth";
+import { useToast } from "hooks/useToast";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { auth } from "utils/firebase";
+import { homePath } from "utils/paths";
+
 const RegistrationConfirmedPage = () => {
-  return <div>ciao</div>;
+  const router = useRouter();
+  const oobCodeQuery = router.query.oobCode;
+  const oobCode = typeof oobCodeQuery === "string" ? oobCodeQuery : oobCodeQuery?.[0] || "";
+
+  const { showToast } = useToast();
+
+  const registerEmail = async () => {
+    try {
+      await applyActionCode(auth, oobCode);
+      showToast("Email verificata correttamente", "success");
+    } catch {
+      showToast("Errore nel verificare l'email", "error");
+    } finally {
+      router.push(homePath);
+    }
+  };
+
+  useEffect(() => {
+    if (oobCode) {
+      registerEmail();
+    }
+  }, [registerEmail, oobCode]);
+
+  return <LoadErrorHandler data={null} error={null} />;
 };
 
 export default RegistrationConfirmedPage;
