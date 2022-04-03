bebfdac068ce062f8fe8ecaa7a345a6201aa6eeb
filippinifra <filippinifra123@gmail.com>
2022-04-04 00:52:17 +0200

    fix useEffect dependency

--- src/pages/registration-confirming.tsx
+++ src/pages/registration-confirming.tsx
@@ -2,7 +2,7 @@ import { LoadErrorHandler } from "components/LoadErrorHandler";
 import { applyActionCode } from "firebase/auth";
 import { useToast } from "hooks/useToast";
 import { useClientRouter } from "hooks/useClientRouter";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { auth } from "utils/firebase";
 import { homePath } from "utils/paths";
 import { useAuth } from "hooks/useAuth";
@@ -16,28 +16,28 @@ const RegistrationConfirmingPage = () => {
 
   const { showToast } = useToast();
 
-  const registerEmail = async () => {
-    setHasBeenCalled(true);
-    try {
-      await applyActionCode(auth, oobCode);
-      if (currentUser) {
-        setCurrentUser((user) => ({ verified: true, email: user?.email || null, uid: user?.uid || "" }));
-      }
-      showToast("Email verificata correttamente", "success");
-    } catch {
-      showToast("Errore nel verificare l'email", "error");
-    } finally {
-      router.push(homePath);
-    }
-  };
-
-  useEffect(() => {
+  const performInitialAction = useCallback(async () => {
     if (oobCode && !hasBeenCalled) {
-      registerEmail();
+      setHasBeenCalled(true);
+      try {
+        await applyActionCode(auth, oobCode);
+        if (currentUser) {
+          setCurrentUser((user) => ({ verified: true, email: user?.email || null, uid: user?.uid || "" }));
+        }
+        showToast("Email verificata correttamente", "success");
+      } catch {
+        showToast("Errore nel verificare l'email", "error");
+      } finally {
+        router.push(homePath);
+      }
     } else {
       router.push(homePath);
     }
-  }, [registerEmail, oobCode, hasBeenCalled, router]);
+  }, [router, oobCode, hasBeenCalled, currentUser, setCurrentUser, showToast]);
+
+  useEffect(() => {
+    performInitialAction();
+  }, [performInitialAction]);
 
   return <LoadErrorHandler data={null} error={null} />;
 };
