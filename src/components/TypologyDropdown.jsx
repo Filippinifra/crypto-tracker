23a1edf89d46393b1570d57e981bf17bbbcaefef
filippinifra <filippinifra123@gmail.com>
2022-04-05 23:21:27 +0200

    fix typology dropdown style

--- src/components/TypologyDropdown.jsx
+++ src/components/TypologyDropdown.jsx
@@ -13,14 +13,25 @@ export const TypologyDropdown = ({ options, value, onChange, ...others }) => {
       placeholder={"Tipologia"}
       {...others}
       styles={{
-        container: (props) => ({ ...props, border: "1px solid black", height: 34 }),
+        container: (props) => ({ ...props, border: "1px solid black", margin: 2, height: 30 }),
         control: () => ({
           display: "flex",
+          padding: 0,
+          margin: 0,
+          height: 30,
+          fontSize: 12,
+          fontFamily: "Roboto",
+          letterSpacing: 2,
+          fontWeight: 600,
         }),
         option: (styles, { data, isSelected }) => ({
           ...styles,
           color: data.color || "black",
           backgroundColor: isSelected ? "#CCCCCC" : "white",
+          fontSize: 12,
+          fontFamily: "Roboto",
+          letterSpacing: 2,
+          fontWeight: 600,
           "&:hover": {
             backgroundColor: "#EBEBEB",
           },
@@ -29,6 +40,11 @@ export const TypologyDropdown = ({ options, value, onChange, ...others }) => {
           ...styles,
           color: data?.color || "black",
         }),
+        input: (styles) => ({
+          ...styles,
+          fontSize: 10,
+        }),
+        valueContainer: (styles) => ({ ...styles, padding: "0px 6px", height: 28 }),
       }}
     />
   );
