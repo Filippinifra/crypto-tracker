44dc099c68eccbd1fbda55ac45e9df4a2d83edb4
Marco <10533178@polimi.it>
2022-04-09 19:59:12 +0200

    Added security headers

--- next.config.js
+++ next.config.js
@@ -1,12 +1,37 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+];
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
   compiler: {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
   images: {
     domains: ["assets.coingecko.com"],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
